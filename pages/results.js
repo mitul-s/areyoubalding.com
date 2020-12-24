bff76e0d9b3615e23234bfa8d3304151fd54330c
Mitul Shah <mitulxshah@gmail.com>
2020-12-24 16:11:10 -0500

    reset bug fix

--- pages/results.js
+++ pages/results.js
@@ -10,12 +10,12 @@ import { useRouter } from "next/router";
 
 
 const ResultsContainer = ({ children }) => {
-    const { setEnd } = useQuiz();
+    const { setEnd, setCurrentQuestion, setScore } = useQuiz();
   return (<Shell>
     <Center h="100vh" direction="column">
       {children}
       <NextLink href="/">
-        <Button onClick={() => setEnd(false)}>Return to start</Button>
+        <Button onClick={() => {setCurrentQuestion(0); setScore(0); setEnd(false)}}>Return to start</Button>
       </NextLink>
     </Center>
   </Shell>)
